37556700896ffb499c449a39dd47b2ae729cf65a
kittiu <kittiu@gmail.com>
2019-11-28 14:58:34 +0700

    Rename description

--- my_library/__manifest__.py
+++ my_library/__manifest__.py
@@ -6,7 +6,7 @@
     'author': "Parth Gajjar",
     'website': "http://www.example.com",
     'category': 'Uncategorized',
-    'version': '12.0.2',
+    'version': '12.0.3',
     'depends': ['base', 'web'],
     'data': [
         'security/groups.xml',
